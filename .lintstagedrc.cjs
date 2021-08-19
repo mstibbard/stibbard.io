6d8b8783cf8437e34ffd790b58012e71e3a2be3d
Matthew Stibbard <matthewstibbard@gmail.com>
2021-08-20 08:44:45 +1000

    install husky and lint-staged

---
+++ .lintstagedrc.cjs
@@ -0,0 +1,5 @@
+module.exports = {
+  './src/**/*.{svelte,ts}': 'yarn check:format',
+  './src/**/*.{svelte,ts}': 'yarn lint',
+  './src/**/*.{svelte,ts}': 'yarn run check'
+};
---
+++ .prettierrc.cjs
@@ -0,0 +1,13 @@
+module.exports = {
+  arrowParens: 'always',
+  plugins: ['prettier-plugin-svelte'],
+  printWidth: 80,
+  semi: true,
+  singleQuote: true,
+  svelteSortOrder: 'options-styles-scripts-markup',
+  svelteStrictMode: false,
+  svelteBracketNewLine: true,
+  svelteIndentScriptAndStyle: true,
+  trailingComma: 'none',
+  useTabs: false
+};
