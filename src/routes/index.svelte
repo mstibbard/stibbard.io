c3394f8bbe23acbe7f20788b330da6014b62c0c5
Matthew Stibbard <matthewstibbard@gmail.com>
2021-08-20 16:23:37 +1000

    added initial styling and index content

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,4 +1,64 @@
-<h1 class="text-3xl text-indigo-600">Welcome to SvelteKit</h1>
-<p>
-  Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation
-</p>
+<section name="Intro" class="py-8">
+  <h1
+    class="mb-4 text-3xl md:text-5xl font-extrabold tracking-tight text-black dark:text-white"
+  >
+    Hi, I'm Matt Stibbard
+  </h1>
+  <div class="prose text-gray-600 dark:text-gray-400 max-w-none">
+    <p>
+      I'm a Management Consultant turned Indie Hacker. I love coffee, finance,
+      and technology. I live in Sydney, Australia. &#127462;&#127482;
+    </p>
+
+    <p>
+      You've stumbled upon my personal site. This is where I keep track of the
+      products I've built and my lessons learned along the way.
+    </p>
+
+    <p>
+      You can also follow me on <a href="https://twitter.com/MatthewStibbard"
+        >Twitter</a
+      >.
+    </p>
+  </div>
+</section>
+
+<section name="Aussie Indie Hackers" class="py-8">
+  <h1
+    class="mb-4 text-2xl md:text-4xl font-bold tracking-tight text-black dark:text-white"
+  >
+    Aussie Indie Hackers
+  </h1>
+  <div class="prose text-gray-600 dark:text-gray-400 max-w-none">
+    <p>
+      Thinking of bootstrapping your own product? Already well on your way? Join
+      the fledgling <a href="https://discord.gg/BHyBgErXnd"
+        >Aussie Indie Hackers Discord</a
+      >. It's a friendly group of likeminded people keen to learn together,
+      share wins, and keep each other accountable.
+    </p>
+    <ul>
+      <li>test</li>
+      <li>test</li>
+      <li>test</li>
+    </ul>
+    <blockquote>
+      Well what do we have here lololol yeah it doesn't work yet
+    </blockquote>
+  </div>
+</section>
+
+<section name="Products" class="py-8">
+  <h1
+    class="mb-4 text-2xl md:text-4xl font-bold tracking-tight text-black dark:text-white"
+  >
+    Products
+  </h1>
+  <div class="prose text-gray-600 dark:text-gray-400 max-w-none">
+    <p>
+      WORK IN PROGRESS - <a href="https://shaper-client.vercel.app/"
+        >Very early preview: Cafe Hero</a
+      >
+    </p>
+  </div>
+</section>
