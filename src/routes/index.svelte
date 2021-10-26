8b5618c3018ed1947dbf036fb50ccc65e8a1769f
Matthew Stibbard <matthewstibbard@gmail.com>
2021-10-26 11:23:57 +1100

    updated intro and aussie founders blurb

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -14,25 +14,21 @@
     class="prose dark:prose-dark text-gray-600 dark:text-gray-400 max-w-none"
   >
     <p>
-      I'm a Management Consultant turned Indie Hacker. I love coffee, finance,
-      and technology. I live in Sydney, Australia. &#127462;&#127482;
+      I love coffee, finance, and technology. I live in Sydney, Australia.
+      &#127462;&#127482;
     </p>
 
     <p>
-      You've stumbled upon my personal site. This is where I keep track of the
-      products I've built and my lessons learned along the way.
+      You've stumbled upon my personal site. This is where I write (very
+      occasionally) on topics that I've learned or find interesting at the time.
     </p>
 
-    <p>
-      Check out my <a href="/learning">Learning</a> page for short snippets and
-      other reference materials. For longform content, check out my
-      <a href="blog">Blog</a>.
-    </p>
+    <p>It is mostly for my own future reference.</p>
 
     <p>
       You can also follow me on <a href="https://twitter.com/MatthewStibbard"
         >Twitter</a
-      >.
+      > if that's your thing.
     </p>
   </div>
 </Section>
@@ -47,45 +43,20 @@
     class="prose dark:prose-dark text-gray-600 dark:text-gray-400 max-w-none dark:max-w-none"
   >
     <p>
-      Thinking of bootstrapping your own product? Already well on your way? Join
-      the fledgling <a href="https://discord.gg/BHyBgErXnd"
-        >Aussie Founders Discord</a
-      >. It's a friendly group of likeminded people keen to learn together,
-      share wins, and keep each other accountable.
+      Aussie Founders is a growing community bringing together & supporting
+      Aussies having a go at their own business.
     </p>
-  </div>
-</Section>
 
-<Section name="Products">
-  <h1
-    class="mb-4 text-2xl md:text-4xl font-bold tracking-tight text-black dark:text-white"
-  >
-    Products
-  </h1>
-  <div class="grid grid-cols-1 sm:grid-cols-2 gap-3">
-    <a href="https://discord.gg/BHyBgErXnd">
-      <Product
-        businessModel="None"
-        launchDate={new Date(2021, 7, 2)}
-        name="Aussie Founders"
-        revenueModel="None"
-        what="Support group for Aussies having a go"
-      /></a
-    >
-    <a href="https://www.rafo.com.au"
-      ><Product
-        businessModel="B2B"
-        launchDate={new Date(2021, 9, 12)}
-        name="RAFO"
-        revenueModel="Sales"
-        what="Remote and flexible jobs for Aussies & Kiwis"
-      /></a
-    >
-    <Product
-      businessModel="B2C"
-      name="CODENAME: PROSPER"
-      revenueModel="Subscription"
-      what="Financial education for young Australians"
-    />
+    <p>
+      Whether you have goals of raising capital & hyper growth, or bootstrapping
+      a side project, you'll find likeminded people to learn with, share the
+      highs and lows, and keep each other accountable.
+    </p>
+
+    <p>
+      If that sounds of interest to you, come and join us on the <a
+        href="https://discord.gg/BHyBgErXnd">Aussie Founders Discord</a
+      >.
+    </p>
   </div>
 </Section>
